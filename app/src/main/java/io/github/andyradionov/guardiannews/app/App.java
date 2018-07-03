3da28f7b2666d05d82f7c9ae2d5ff21d9419884c
Andrey Radionov <andyomsk@gmail.com>
2018-07-03 16:35:46 +0300

    Refactoring

--- app/src/main/java/io/github/andyradionov/guardiannews/app/App.java
+++ app/src/main/java/io/github/andyradionov/guardiannews/app/App.java
@@ -19,9 +19,9 @@ import retrofit2.converter.gson.GsonConverterFactory;
  */
 
 public class App extends Application {
-
+    public static final String API_KEY = "test";
     public static final String NEWS_BASE_URL = "http://content.guardianapis.com";
-    public static final String SEARCH_NEWS_QUERY = "search?api-key=test&page-size=30&order-by=newest&show-fields=thumbnail,trailText";
+    public static final String SEARCH_NEWS_QUERY = "search?api-key=" + API_KEY + "&page-size=30&order-by=newest&show-fields=thumbnail,trailText";
     public static final String ALL_NEWS_QUERY = "";
 
     private static final String TAG = App.class.getSimpleName();
